aeefbe07ad68fc53b024f911b6ca98cab978a6c0
Mike Oksenenko <mmx.mmx@gmail.com>
2017-07-09 03:36:38 +0300

    Create TestJds.java

---
+++ src/test/java/mikelaud/jds/TestJds.java
@@ -0,0 +1,14 @@
+package mikelaud.jdf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestJds {
+
+	@Test
+	public void test() {
+		assertTrue(JitpackDemoSecond.exec());
+	}
+	
+}
